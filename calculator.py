5ed5ff20d6b32176c971c434f5636de7f0259471
Tsukr <i847619@gmail.com>
2023-02-28 16:52:09 +0200

    add recive_numb funсtion

--- calculator.py
+++ calculator.py
@@ -1,8 +1,15 @@
 def main():
-    ...
+    num1=recive_numb()
+    print(num1)
 
 def recive_numb():
-    ...
+    '''filtr input. recive only number'''
+    while True:
+        num = input()
+        if num.isdigit():
+            break
+    return num
+
 def add(a,b):
     ...
 
